60cd44c188f14b386de3f0cc2b090817e9c28439
Rowan Stein <rowan.stein@agyn.io>
2026-05-26 06:08:41 +0400

    Revert "fix(redirect): resolve span-id message links (#48)"
    This reverts commit 9ad2412965c4c22035aaa9ec6898d214c8b1ae89.

--- src/api/modules/runs.ts
+++ src/api/modules/runs.ts
@@ -228,24 +228,14 @@ export const runs = {
     organizationId: string,
     messageId: string,
   ): Promise<{ runId: string } | null> => {
-    let resp = await tracingClient.listSpans({
+    const resp = await tracingClient.listSpans({
       organizationId,
       filter: { messageId },
       pageSize: 1,
       pageToken: '',
       orderBy: ListSpansOrderBy.START_TIME_DESC,
     });
-    let spans = flattenResourceSpans(resp.resourceSpans);
-    if (spans.length === 0) {
-      resp = await tracingClient.listSpans({
-        organizationId,
-        filter: { names: [INVOCATION_MESSAGE_SPAN_NAME] },
-        pageSize: 500,
-        pageToken: '',
-        orderBy: ListSpansOrderBy.START_TIME_DESC,
-      });
-      spans = flattenResourceSpans(resp.resourceSpans).filter(({ span }) => bytesToHex(span.spanId) === messageId);
-    }
+    const spans = flattenResourceSpans(resp.resourceSpans);
     if (spans.length === 0) return null;
     return { runId: bytesToHex(spans[0].span.traceId) };
   },
